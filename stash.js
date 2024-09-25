2dd1446c3b056703c4001a9afc6a0874d11f991f
chimdi ikechukwu <chimdiikechukwu00@gmail.com>
2024-09-26 00:49:00 +0100

    completing the UI

---
+++ stash.js
@@ -0,0 +1,8 @@
+
+itemsBought = document.querySelectorAll('.spending');
+costOfGoods = document.querySelectorAll('.cost-reduce');
+
+addMoreElement = document.querySelectorAll('.add-items');
+dailyTotal = document.querySelectorAll('.daily-total');
+
+totalSpentDaily = document.querySelectorAll('.what___i__spent__daily');
