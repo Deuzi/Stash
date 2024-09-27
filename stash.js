37594be1d8c8ef9557978223ec900cff1b1b811c
chimdi ikechukwu <chimdiikechukwu00@gmail.com>
2024-09-27 01:00:15 +0100

    first-flawed-functionality

--- stash.js
+++ stash.js
@@ -1,8 +1,39 @@
 
-itemsBought = document.querySelectorAll('.spending');
-costOfGoods = document.querySelectorAll('.cost-reduce');
+const itemsBought = document.querySelector('.spending');
+const costOfGoods = document.querySelector('.cost-reduce');
 
-addMoreElement = document.querySelectorAll('.add-items');
-dailyTotal = document.querySelectorAll('.daily-total');
+const addElement = document.querySelector('.add-items');
+const dailyTotal = document.querySelectorAll('.daily-total');
 
-totalSpentDaily = document.querySelectorAll('.what___i__spent__daily');
+const totalSpentDaily = document.querySelectorAll('.what___i__spent__daily');
+
+const costHolder = document.querySelector('.cost');
+const itemHolder = document.querySelector('.items');
+
+
+addElement.addEventListener  ('click', function(){
+
+    const innerItems = document.createElement('p');
+    innerItems.innerText = 'item bought';
+
+    const innerItemInput = document.createElement('input');
+    innerItemInput.className = 'spending';
+
+    
+    //   
+    const innerCost = document.createElement('div');
+    innerCost.innerText = 'cost';
+    
+    const innerCostInput = document.createElement('input');
+    innerCostInput.className = 'cost-reduce';
+
+    itemHolder.appendChild(innerItems);
+    itemHolder.appendChild (innerItemInput);
+    
+    
+    costHolder.appendChild(innerCost);
+    costHolder.appendChild (innerCostInput);
+    
+})
+
+
